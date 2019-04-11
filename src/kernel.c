3b174417123af3c400c2754d8937091305a819a3
Veyxos <veyxos@gmail.com>
2019-04-11 15:01:25 +0200

    add mini_uart.c c1.1

--- src/kernel.c
+++ src/kernel.c
@@ -2,7 +2,7 @@
 
 void kernel_main(void) {
 	uart_init();
-	uart_send_string("Hello World\r\n");
+	uart_send_string("\"Portability is for people who cannot write new programs.\"\r\n- Linus Torvalds, 29.01.1992\r\n");
 
 	while (1) {
 		uart_send(uart_recv());
